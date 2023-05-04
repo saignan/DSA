fc06b891ab51cbd646057868ce8c23fb153bdb23
saignan <saignankarrolla695@gmail.com>
2023-05-04 09:47:39 +0530

    programs added

---
+++ Algorithms/Algorithms/Algorithm/Algorithm/DecimalToBinary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class DecimalToBinary
+    {
+        public void  decimalToBinary()
+        {
+            Console.Write("Please enter the decimal value: ");
+            int dec = Convert.ToInt32(Console.ReadLine());
+
+
+            String binary = toBinary(dec);
+
+            Console.WriteLine(binary);
+        }
+
+        private static string toBinary(int dec)
+        {
+            string stnum = "";
+            while (dec > 0)
+            {
+                stnum = stnum + dec % 2;
+                dec /= 2;
+            }
+
+            string binary = "";
+            for (int i = stnum.Length - 1; i >= 0; i--)
+            {
+                binary = binary + stnum[i];
+            }
+
+            return binary;
+        }
+    
+    }
+}
